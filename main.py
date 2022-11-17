5ce39963f1e090a10a37f40849f4df2ad1f42104
DIvkov575 <Dmitriy@Ivkov.net>
2022-11-16 22:01:41 -0500

    arbitrary1.2 incomplete

--- main.py
+++ main.py
@@ -11,22 +11,42 @@ def arbitrary():
   
     # if (a == m):
     #     return 0
-    a = 126
-    k = 1000000
+    a = 253
+    k = 3425
     m = 250
     b = 1 
     start = time.time()
-    
     a %= m
-
-
     for _ in range(k):
         b = (b * a)%m
     print(b)
-
-
     print(time.time() - start)
+    
 
+    a = 253
+    k = 3425
+    m = 250
+    b = 1
+
+    a %= m
+    start2 = time.time()
+   
+    def recurse(a,b,k):
+        if float(k) <= 0:
+            return b%m
+        if k%2==0:
+            k /= 2
+            a **=2
+            a %=m
+            b = (b*a)%m
+            return recurse(a,b,k)
+        if k%2==1:
+            return (a * recurse(a,b,k-1))%m
+    b = recurse(a,b,k)
+    print(b)
+    print(time.time() - start2)
+
+# 0.08165407
 
 
 
