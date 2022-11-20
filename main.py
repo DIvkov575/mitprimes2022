cff7627ae92004d08b546fdd8af02f2eec77c65a
DIvkov575 <Dmitriy@Ivkov.net>
2022-11-20 14:38:13 -0500

    partial encryption/decryption (has binary exponentiation)

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-def part1(p, q, e):
+def encrypt(n, e, m):
+    c = (m**e)%n
+    return c
 
-    def EEA(k, m):
-        prev_coefficient, coefficient_a = 1, 0
-        prev_coefficient_b, coefficient_b = 0, 1
-        while (m != 0):
-            q = k // m  # find number of mod that can 'fit' within provided number
-            k, m = m, k - q*m # finds remainder part of mod operation
-            prev_coefficient_b, coefficient_b = coefficient_b, prev_coefficient_b - q*coefficient_b
+def decryption(p, q, d, c):
+    def binary_expo(a, b, n):
+        if b <= 1:
+            return a%n
 
-        return prev_coefficient, prev_coefficient*k
+        if b%2 == 0:
+            tmp = binary_expo(a, b/2, n)
+            return (tmp**2)%n
 
-        
-    d, coeff_low = EEA(e, (p-1)*(q-1))
+        if b%2 == 1:
+            tmp = binary_expo(a, (b-1)/2, n)
+            return (tmp**2 * a)%n
 
-    if coeff_low != 1:
-        print("ValueError: phi(n) and encryption-exponent passed are not coprime")
-        return
-    
-    
-
-    print(f"public (n, e): {p*q}, {e}")
-    print(f"private (p, q, d): {p}, {q}, {d}")
+    # binary_expo(c, d, p*q)
+    answer = binary_expo(295, 317, 437)
+    return answer
 
