193682ac4638fd3bba4bfd4cb9a797c77f723fdd
DIvkov575 <Dmitriy@Ivkov.net>
2022-11-19 23:54:08 -0500

    2.5.1

--- main.py
+++ main.py
@@ -1,54 +1,24 @@
-import time
-a = 992
-k = 9212343234
-m = 1000
+def part1(p, q, e):
 
+    def EEA(k, m):
+        prev_coefficient, coefficient_a = 1, 0
+        prev_coefficient_b, coefficient_b = 0, 1
+        while (m != 0):
+            q = k // m  # find number of mod that can 'fit' within provided number
+            k, m = m, k - q*m # finds remainder part of mod operation
+            prev_coefficient_b, coefficient_b = coefficient_b, prev_coefficient_b - q*coefficient_b
 
-def arbitrary():
-    # a = int(input("a: "))
-    # k = int(input("k: "))
-    # m = int(input("m: "))
-  
-    # if (a == m):
-    #     return 0
-    a = 253
-    k = 3425
-    m = 250
-    b = 1 
-    start = time.time()
-    a %= m
-    for _ in range(k):
-        b = (b * a)%m
-    print(b)
-    print(time.time() - start)
+        return prev_coefficient, prev_coefficient*k
+
+        
+    d, coeff_low = EEA(e, (p-1)*(q-1))
+
+    if coeff_low != 1:
+        print("ValueError: phi(n) and encryption-exponent passed are not coprime")
+        return
+    
     
 
-    a = 253
-    k = 3425
-    m = 250
-    b = 1
+    print(f"public (n, e): {p*q}, {e}")
+    print(f"private (p, q, d): {p}, {q}, {d}")
 
-    a %= m
-    start2 = time.time()
-   
-    def recurse(a,b,k):
-        if float(k) <= 0:
-            return b%m
-        if k%2==0:
-            k /= 2
-            a **=2
-            a %=m
-            b = (b*a)%m
-            return recurse(a,b,k)
-        if k%2==1:
-            return (a * recurse(a,b,k-1))%m
-    b = recurse(a,b,k)
-    print(b)
-    print(time.time() - start2)
-
-# 0.08165407
-
-
-
-
-arbitrary()
