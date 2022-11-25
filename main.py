60bbca23bd34152db546ae5c204afc4a459bcdff
DIvkov575 <Dmitriy@Ivkov.net>
2022-11-25 00:00:28 -0500

    finished 5.5 blind signatures

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import random
 
-def blind_signature(m, r, e, d, n):
-    answer = pow(m*(r**e), d, n)
+def signature_validation(m, d, n, e):
+    def blind_signature(m, e, n):
+        r = random.randint(1, n//m)
+        return m*(r**e), r
+    def extract_signature(m, d, n):
+        return pow(m, d, n)
+    
+    b_sig, r = blind_signature(m, e, n)
+    blind_signature = extract_signature(m, d, n)
+    signature = blind_signature/r
 
-    return answer
-
-print(blind_signature(25, 15, 25, 15, 40))
+signature_validation(25, 317, 437, 5)
